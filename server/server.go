cdff4769b39fb3860c4e72797569d482ee4e8b16
Samer Masterson <samer@samertm.com>
2014-04-05 16:57:45 -0400

    Added /about and /contact routes.

--- server/server.go
+++ server/server.go
@@ -18,8 +18,20 @@ func serveFood(w http.ResponseWriter, req *http.Request) {
 
 }
 
+func serveAbout(w http.ResponseWriter, req *http.Request) {
+	t, _ := template.ParseFiles("templates/about.html")
+	t.Execute(w, nil)
+}
+
+func serveContact(w http.ResponseWriter, req *http.Request) {
+	t, _ := template.ParseFiles("templates/contact.html")
+	t.Execute(w, nil)
+}
+
 func ListenAndServe(ip string, f foodstore.FoodStore) {
 	fmt.Printf("Running server on %s.\n", ip)
 	http.HandleFunc("/", serveRoot)
+	http.HandleFunc("/about", serveAbout)
+	http.HandleFunc("/contact", serveContact)
 	log.Fatal(http.ListenAndServe(ip, nil))
 }
