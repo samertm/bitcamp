22441c0f840f2d61dc406cc3e7af4148ebce30a0
Samer Masterson <samer@samertm.com>
2014-04-05 19:59:49 -0400

    Hack: Fix template with FoodWrapper

--- server/server.go
+++ server/server.go
@@ -16,10 +16,15 @@ func serveRoot(w http.ResponseWriter, req *http.Request) {
 	t.Execute(w, nil)
 }
 
+type FoodWrapper struct {
+	Foods []foodstore.Food
+}
+
 func serveFood(w http.ResponseWriter, req *http.Request) {
 	foods := runAlgorithm(15)
+	wrapper := FoodWrapper{foods}
 	t, _ := template.ParseFiles("templates/food.html")
-	t.Execute(w, foods)
+	t.Execute(w, wrapper)
 }
 
 func serveAbout(w http.ResponseWriter, req *http.Request) {
@@ -38,7 +43,7 @@ func runAlgorithm(money int) []foodstore.Food {
 		store["brown rice"],
 	}
 }
-	
+
 func ListenAndServe(ip string, f foodstore.FoodStore) {
 	store = f
 	fmt.Printf("Running server on %s.\n", ip)
