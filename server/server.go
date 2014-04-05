eaca6eea2dea3399e4f5240fec87bf03a6abe7f4
Samer Masterson <samer@samertm.com>
2014-04-05 19:35:27 -0400

    Added food template.

--- server/server.go
+++ server/server.go
@@ -9,13 +9,17 @@ import (
 	"github.com/samertm/bitcamp/foodstore"
 )
 
+var store foodstore.FoodStore
+
 func serveRoot(w http.ResponseWriter, req *http.Request) {
 	t, _ := template.ParseFiles("templates/index.html")
 	t.Execute(w, nil)
 }
 
 func serveFood(w http.ResponseWriter, req *http.Request) {
-
+	foods := runAlgorithm(15)
+	t, _ := template.ParseFiles("templates/food.html")
+	t.Execute(w, foods)
 }
 
 func serveAbout(w http.ResponseWriter, req *http.Request) {
@@ -28,7 +32,15 @@ func serveContact(w http.ResponseWriter, req *http.Request) {
 	t.Execute(w, nil)
 }
 
+func runAlgorithm(money int) []foodstore.Food {
+	return []foodstore.Food{
+		store["white rice"],
+		store["brown rice"],
+	}
+}
+	
 func ListenAndServe(ip string, f foodstore.FoodStore) {
+	store = f
 	fmt.Printf("Running server on %s.\n", ip)
 	http.HandleFunc("/", serveRoot)
 	http.HandleFunc("/about/", serveAbout)
