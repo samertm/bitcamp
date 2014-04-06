c028fc83af08cb75817a35902e7dc6d52197818c
Samer Masterson <samer@samertm.com>
2014-04-06 04:14:52 -0400

    fixed main

--- main.go
+++ main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"github.com/samertm/bitcamp/foodstore"
-	_ "github.com/samertm/bitcamp/server"
-
-	//DEBUG
-	"fmt"
+	"github.com/samertm/bitcamp/server"
 )
 
 func main() {
 	foodstore.NewFromDb() // TODO change to init
-	fmt.Println(foodstore.OptimalFoods(10.0))
-	fmt.Println(foodstore.Sortedfoods.Calories)
-        //server.ListenAndServe(":8889", f)
+        server.ListenAndServe(":8889", f)
 }
